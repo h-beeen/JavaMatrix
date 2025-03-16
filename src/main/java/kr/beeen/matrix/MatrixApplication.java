e26bc0fe034dde4287a946bdeeaf8faf847576bc
Haebin <joker7011@naver.com>
2025-03-16 17:32:36 +0900

    chore: initial commit

---
+++ src/main/java/kr/beeen/matrix/MatrixApplication.java
@@ -0,0 +1,13 @@
+package kr.beeen.matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/beeen/matrix/MatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.beeen.matrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
